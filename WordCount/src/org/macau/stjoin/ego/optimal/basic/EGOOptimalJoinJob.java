4a8a78f9ad65c17fa91673e41ada54ee7e511e32
mb25428 <mb25428@FST-259QNLQONGN>
2015-05-22 22:38:05 +0800

    The EGOP Optimal Join

--- WordCount/src/org/macau/stjoin/ego/optimal/basic/EGOOptimalJoinJob.java
+++ WordCount/src/org/macau/stjoin/ego/optimal/basic/EGOOptimalJoinJob.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.macau.flickr.job.TemporalSimilarityJoin;
+
 import org.macau.flickr.util.FlickrSimilarityUtil;
 import org.macau.flickr.util.FlickrValue;
 
@@ -24,18 +24,15 @@ public class EGOOptimalJoinJob {
 	public static boolean EGOOptimalJoin(Configuration conf,int reducerNumber) throws Exception{
 		
 		Job basicJob = new Job(conf,"EGO optimal Join");
-		basicJob.setJarByClass(TemporalSimilarityJoin.class);
+		basicJob.setJarByClass(EGOOptimalJoinJob.class);
 		
 		basicJob.setMapperClass(EGOOptimalJoinMapper.class);
-//		basicJob.setCombinerClass(TemporalJoinReducer.class);
 		
 		basicJob.setReducerClass(EGOOptimalJoinReducer.class);
 		
 		basicJob.setMapOutputKeyClass(LongWritable.class);
 		basicJob.setMapOutputValueClass(FlickrValue.class);
 		
-//		basicJob.setOutputKeyClass(Text.class);
-//		basicJob.setOutputValueClass(Text.class);
 		basicJob.setNumReduceTasks(reducerNumber);
 		
 		FileInputFormat.addInputPath(basicJob, new Path(FlickrSimilarityUtil.flickrInputPath));
