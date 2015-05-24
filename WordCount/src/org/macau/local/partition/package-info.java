6b6c06d989ddf5da39636b805a0eecb1a8761481
mb25428 <mb25428@FST-259QNLQONGN>
2015-05-24 23:34:07 +0800

    The partition code

---
+++ WordCount/src/org/macau/local/partition/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author mb25428
+ * 
+ * read the data information and get the partition plan.
+ *
+ */
+package org.macau.local.partition;
---
+++ WordCount/src/org/macau/local/selectivity/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author mb25428
+ * 
+ * Modified: 2015-05-24
+ * For the Selectivity of the join
+ *
+ */
+package org.macau.local.selectivity;
