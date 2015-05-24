2aa1ab3440d3f6ee0b08f8c8c5be20c96b1c26ce
mb25428 <mb25428@FST-259QNLQONGN>
2015-05-24 23:44:33 +0800

    the analysis the time of different feature

---
+++ WordCount/src/org/macau/local/analysis/time/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author mb25428
+ * 
+ * This package analysis the time of different feature
+ * 
+ * Because I want to know the relationship of unit cost and the selectivity
+ * So I use different selectivity and different feature order to test the influence of
+ * selectivity based on that we know the unit cost.
+ *
+ */
+package org.macau.local.analysis.time;
