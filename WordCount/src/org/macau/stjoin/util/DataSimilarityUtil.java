1baab972b889255c2113711e8f363d4332faa097
mb25428 <mb25428@FST-259QNLQONGN>
2015-06-10 19:53:49 +0800

    add new function for the new flickr type

--- WordCount/src/org/macau/stjoin/util/DataSimilarityUtil.java
+++ WordCount/src/org/macau/stjoin/util/DataSimilarityUtil.java
@@ -1,6 +1,7 @@
 package org.macau.stjoin.util;
 
 import org.macau.flickr.util.FlickrValue;
+import org.macau.flickr.util.FlickrValueWithMultiFeature;
 
 public class DataSimilarityUtil {
 
@@ -13,9 +14,6 @@ public class DataSimilarityUtil {
 		String others = value.toString().split(":")[6];
 		
 		
-		
-		
-		
 		outputValue.setId(id);
 		outputValue.setLat(lat);
 		outputValue.setLon(lon);
@@ -23,6 +21,36 @@ public class DataSimilarityUtil {
 		outputValue.setTiles(textual);
 		outputValue.setOthers(others);
 	}
+	
+	public static void getFlickrValue(FlickrValueWithMultiFeature outputValue, String value){
+		long id =Long.parseLong(value.toString().split(":")[0]);
+		double lat = Double.parseDouble(value.toString().split(":")[2]);
+		double lon = Double.parseDouble(value.toString().split(":")[3]);
+		long timestamp = Long.parseLong(value.toString().split(":")[4]);
+		String textual = value.toString().split(":")[5];
+		String others = value.toString().split(":")[6];
+		
+		
+		String candidateTags = "";
+		long uploadtime = Long.parseLong(value.toString().split(":")[1]);
+		long serverId = Long.parseLong(value.toString().split(":")[7]);;
+		String device = value.split(":")[8];
+		String description = value.split(":")[8];
+		
+		outputValue.setId(id);
+		outputValue.setLat(lat);
+		outputValue.setLon(lon);
+		outputValue.setTimestamp(timestamp);
+		outputValue.setTiles(textual);
+		outputValue.setOthers(others);
+		outputValue.setCandidateTags(candidateTags);
+		outputValue.setTileTag("");
+		outputValue.setUploadtime(uploadtime);
+		outputValue.setServerId(serverId);
+		outputValue.setDevice(device);
+		outputValue.setDescription(description);
+	}
+	
 	
 	public static FlickrValue getFlickrValue(String value){
 		
