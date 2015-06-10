540c3e3b0f316f97da5d08ac62547493ee1bdcc1
mb25428 <mb25428@FST-259QNLQONGN>
2015-06-10 19:55:42 +0800

    use String to define new partition point

---
+++ WordCount/src/org/macau/local/test/StringOutputTest.java
@@ -0,0 +1,10 @@
+package org.macau.local.test;
+
+public class StringOutputTest {
+
+	public static void main(String[] args){
+		//data size and threshold is 7
+		String input = "736,1256,1302,1326,1337,1348,1358,1365,1370,1376,1382,1388,1394,1399,1404,1408,1413,1418,1424,1429,1434,1438,1442,1445,1449,1453,1459,1465,1471,1477";
+		String[] inputArray = input.split(",");
+	}
+}
