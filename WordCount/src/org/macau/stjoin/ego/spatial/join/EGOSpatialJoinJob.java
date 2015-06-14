d07c829338b831e991653fbd2e0b287f8fff7d31
mb25428 <mb25428@FST-259QNLQONGN>
2015-06-14 17:14:53 +0800

    ego spatial

---
+++ WordCount/src/org/macau/stjoin/ego/spatial/join/EGOSpatialJoinJob.java
@@ -0,0 +1,46 @@
+package org.macau.stjoin.ego.spatial.join;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import org.macau.flickr.util.FlickrSimilarityUtil;
+import org.macau.flickr.util.FlickrValue;
+
+
+/**
+ * 
+ * @author hadoop
+ * EGO Optimal Join
+ * Find the min max reducer time partition solution to do the join operation
+ * because we find the optimal partition solution
+ * 
+ */
+public class EGOSpatialJoinJob {
+
+	public static boolean EGOSpatialJoin(Configuration conf,int reducerNumber) throws Exception{
+		
+		Job basicJob = new Job(conf,"EGO optimal Join");
+		basicJob.setJarByClass(EGOSpatialJoinJob.class);
+		
+		basicJob.setMapperClass(EGOSpatialJoinMapper.class);
+		
+		basicJob.setReducerClass(EGOSpatialJoinReducer.class);
+		
+		basicJob.setMapOutputKeyClass(LongWritable.class);
+		basicJob.setMapOutputValueClass(FlickrValue.class);
+		
+		basicJob.setNumReduceTasks(reducerNumber);
+		
+		FileInputFormat.addInputPath(basicJob, new Path(FlickrSimilarityUtil.flickrInputPath));
+		FileOutputFormat.setOutputPath(basicJob, new Path(FlickrSimilarityUtil.flickrOutputPath));
+		
+		if(basicJob.waitForCompletion(true))
+			return true;
+		else
+			return false;
+	}
+}
